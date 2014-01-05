2c2748c1b267a0de6aa5a71adc5feb5130b2801d
Lindsay Thurmond <lindsay.thurmond@gmail.com>
2014-01-05 17:16:55 -0500

    change data to save in local storage

--- site/store.js
+++ site/store.js
@@ -2,4 +2,7 @@
  * Created by Thurmond on 12/30/13.
  */
 
-App.ApplicationAdapter = DS.FixtureAdapter;
+//App.ApplicationAdapter = DS.FixtureAdapter;
+
+// use local storage
+App.ApplicationAdapter = DS.LSAdapter;
